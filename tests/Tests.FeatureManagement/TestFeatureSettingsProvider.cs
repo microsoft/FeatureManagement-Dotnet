19643e742d093e2f9fee90244b28ad81c06842f5
Stephen Lomangino <slomangino@flightdocs.com>
2020-04-20 09:44:39 -0400

    Created some test classes.

---
+++ tests/Tests.FeatureManagement/TestFeatureSettingsProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.FeatureManagement;
+
+namespace Tests.FeatureManagement
+{
+    /// <summary>
+    /// Test Implementation of IFeatureSettingsProvider.
+    /// </summary>
+    public class TestFeatureSettingsProvider : IFeatureSettingsProvider
+    {
+        public TestThirdPartyFeatureSettingsService FeatureSettingsService { get; set; }
+
+        public TestFeatureSettingsProvider()
+        {
+            FeatureSettingsService = new TestThirdPartyFeatureSettingsService();
+        }
+
+        public async IAsyncEnumerable<FeatureSettings> GetAllFeatureSettingsAsync()
+        {
+            var features = await FeatureSettingsService.GetAllFeaturesAsync();
+            foreach (var feature in features.Select(x => new FeatureSettings() { Name = x }))
+            {
+                yield return feature;
+            }
+        }
+
+        public async Task<FeatureSettings> GetFeatureSettingsAsync(string featureName)
+        {
+            if (string.IsNullOrWhiteSpace(featureName))
+            {
+                throw new ArgumentNullException(nameof(featureName));
+            }
+
+            bool fetchedSetting = await FeatureSettingsService.IsEnabledAsync(featureName);
+            
+            var filterSettings = new List<FeatureFilterSettings>();
+            
+            var setting = new FeatureSettings()
+            {
+                Name = featureName,
+            };
+
+            if (fetchedSetting)
+            {
+                filterSettings.Add(new FeatureFilterSettings()
+                {
+                    Name = "AlwaysOn",
+                });
+
+                setting.EnabledFor = filterSettings;
+            }
+
+            return setting;
+        }
+    }
+}
---
+++ tests/Tests.FeatureManagement/TestThirdPartyFeatureSettingsService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.FeatureManagement
+{
+    /// <summary>
+    /// Test Implementation of a service responsible for retrieving feature settings from a third party.
+    /// </summary>
+    public class TestThirdPartyFeatureSettingsService
+    {
+        public Task<bool> IsEnabledAsync(string feature)
+        {
+            var isEnabled = false;
+            Enum.TryParse(feature, out Features featureEnum);
+
+            switch (featureEnum)
+            {
+                case Features.OnTestFeature:
+                    isEnabled = true;
+                    break;
+
+                case Features.OffTestFeature:
+                    break;
+            }
+
+            return Task.FromResult(isEnabled);
+        }
+
+        public Task<IEnumerable<string>> GetAllFeaturesAsync()
+        {
+            var features = Enum.GetValues(typeof(Features)).Cast<Features>().Select(x => x.ToString());
+            return Task.FromResult(features);
+        }
+    }
+}
