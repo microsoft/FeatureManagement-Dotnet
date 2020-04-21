01b8521bc1a44a98a33d25bcd4f41a549ef89258
Jimmy Campbell <jimmyca@microsoft.com>
2020-04-21 13:53:36 -0700

    Fixed build and pack based to respect updated .NET Core tooling behavior.

--- pack.ps1
+++ pack.ps1
@@ -15,15 +15,26 @@ $ErrorActionPreference = "Stop"
 $PrebuiltBinariesDir = "bin\BuildOutput"
 $PublishRelativePath = "bin\PackageOutput"
 $LogDirectory = "$PSScriptRoot\buildlogs"
-$Solution     = "$PSScriptRoot\Microsoft.FeatureManagement.sln"
+
+$targetProjects = @(
+
+    "Microsoft.FeatureManagement",
+    "Microsoft.FeatureManagement.AspNetCore"
+)
 
 # Create the log directory.
 if ((Test-Path -Path $LogDirectory) -ne $true) {
     New-Item -ItemType Directory -Path $LogDirectory | Write-Verbose
 }
 
-#
-# The build system expects pre-built binaries to be in the folder pointed to by 'OutDir'.
-dotnet pack -o "$PublishRelativePath" /p:OutDir=$PrebuiltBinariesDir "$Solution" --no-build | Tee-Object -FilePath "$LogDirectory\build.log"
+foreach ($project in $targetProjects)
+{
+    $projectPath = "$PSScriptRoot\src\$project\$project.csproj"
+    $outputPath = "$PSScriptRoot\src\$project\$PublishRelativePath"
+
+    #
+    # The build system expects pre-built binaries to be in the folder pointed to by 'OutDir'.
+    dotnet pack -o "$outputPath" /p:OutDir=$PrebuiltBinariesDir "$projectPath" --no-build | Tee-Object -FilePath "$LogDirectory\build.log"
+}
 
 exit $LASTEXITCODE
