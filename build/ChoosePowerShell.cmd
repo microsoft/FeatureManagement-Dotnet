8db92581f457c1eed8bb1ece8d11703d517b137e
Jimmy Campbell <jimmyca@microsoft.com>
2020-07-10 12:47:56 -0700

    Reversed order of PowerShell selection.

--- build/ChoosePowerShell.cmd
+++ build/ChoosePowerShell.cmd
@@ -1,19 +1,19 @@
 :: where.exe does not exist in windows container, application specific test must be used to check for existence
 
-PowerShell -Command Write-Host "a"
-
-IF %ERRORLEVEL% == 0 (
-
-    set PowerShell=PowerShell
-
-    exit /B 0
-)
-
 pwsh -Command Write-Host "a"
 
 IF %ERRORLEVEL% == 0 (
 
     set PowerShell=pwsh
+
+    exit /B 0
+)
+
+PowerShell -Command Write-Host "a"
+
+IF %ERRORLEVEL% == 0 (
+
+    set PowerShell=PowerShell
 
     exit /B 0
 )
