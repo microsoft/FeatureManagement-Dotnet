e3eb4dacc4b8238c23a8265bdb288ac72c2a3c10
Neil Johari <neil@johari.tech>
2021-12-03 15:12:24 -0800

    Fix PercentageFilter inequality bound (#156)
    NextDouble() ∈ [0, 1). We use a strict inequality, so a 0%
    value sporadically returns true as 0⩽0.
    
    The fix is to use a non-strict inequality, <. The upper bound still works: 0.999... < 1.

--- src/Microsoft.FeatureManagement/FeatureFilters/PercentageFilter.cs
+++ src/Microsoft.FeatureManagement/FeatureFilters/PercentageFilter.cs
@@ -46,7 +46,7 @@ namespace Microsoft.FeatureManagement.FeatureFilters
 
             if (result)
             {
-                result = (RandomGenerator.NextDouble() * 100) <= settings.Value;
+                result = (RandomGenerator.NextDouble() * 100) < settings.Value;
             }
 
             return Task.FromResult(result);
