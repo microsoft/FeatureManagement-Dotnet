74f5aea9dd238aa2a65f708bf159267b986d5a4e
Avani Gupta <avgupta@microsoft.com>
2023-02-24 09:22:54 -0800

    install net 5 for CI/CD

--- build/install-dotnet.ps1
+++ build/install-dotnet.ps1
@@ -1,8 +1,10 @@
-# Installs .NET Core 2.1 and .NET 6 for CI/CD environment
+# Installs .NET Core 2.1, .NET 5 and .NET 6 for CI/CD environment
 # see: https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script#examples
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
 &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Version 2.1.816
+
+&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Version 5.0.17
 
 &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1')))
