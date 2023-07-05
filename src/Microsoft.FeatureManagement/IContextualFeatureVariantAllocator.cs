703f89a77f2577cf8f3a2e5435670e8fd7c84aa0
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2023-07-05 14:51:31 -0500

    progress adding getvariant logic classes

---
+++ src/Microsoft.FeatureManagement/IContextualFeatureVariantAllocator.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.FeatureManagement
+{
+    /// <summary>
+    /// Provides a method to allocate a variant of a feature to be used based off of custom conditions.
+    /// </summary>
+    /// <typeparam name="TContext">A custom type that the allocator requires to perform allocation</typeparam>
+    public interface IContextualFeatureVariantAllocator<TContext> : IFeatureVariantAllocatorMetadata
+    {
+        /// <summary>
+        /// Allocate a variant of a feature to be used based off of customized criteria.
+        /// </summary>
+        /// <param name="variantAllocationContext">A variant allocation context that contains information needed to allocate a variant for a feature.</param>
+        /// <param name="appContext">A context defined by the application that is passed in to the feature management system to provide contextual information for allocating a variant of a feature.</param>
+        /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+        /// <returns>The variant that should be allocated for a given feature.</returns>
+        ValueTask<FeatureVariant> AllocateVariantAsync(FeatureVariantAllocationContext variantAllocationContext, TContext appContext, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Microsoft.FeatureManagement/IFeatureVariantAllocator.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.FeatureManagement
+{
+    /// <summary>
+    /// Provides a method to allocate a variant of a dynamic feature to be used based off of custom conditions.
+    /// </summary>
+    public interface IFeatureVariantAllocator : IFeatureVariantAllocatorMetadata
+    {
+        /// <summary>
+        /// Allocate a variant of a dynamic feature to be used based off of customized criteria.
+        /// </summary>
+        /// <param name="variantAllocationContext">A variant allocation context that contains information needed to allocate a variant for a dynamic feature.</param>
+        /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+        /// <returns>The variant that should be allocated for a given dynamic feature.</returns>
+        ValueTask<FeatureVariant> AllocateVariantAsync(FeatureVariantAllocationContext variantAllocationContext, CancellationToken cancellationToken = default);
+    }
+}
