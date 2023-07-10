0a4dcfb840c5dd5a226590cb76725de637de739b
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2023-07-10 11:31:58 -0500

    add snapshot changes

--- src/Microsoft.FeatureManagement/FeatureManager.cs
+++ src/Microsoft.FeatureManagement/FeatureManager.cs
@@ -10,7 +10,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Tasks;
 
