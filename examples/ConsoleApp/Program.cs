f189d9f14c46742e4ca8f30dc7b5d95bf12fb8ec
Ross Grambo <rossgrambo@microsoft.com>
2023-10-12 11:29:31 -0700

    Recreates examples with latest packages (#277)

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,70 +1,54 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-//
-using Consoto.Banking.AccountService.FeatureManagement;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FeatureManagement;
 using Microsoft.FeatureManagement.FeatureFilters;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace Consoto.Banking.AccountService
+//
+// Setup configuration
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+//
+// Setup application services + feature management
+IServiceCollection services = new ServiceCollection();
+
+services.AddSingleton(configuration)
+        .AddFeatureManagement()
+        .AddFeatureFilter<PercentageFilter>()
+        .AddFeatureFilter<AccountIdFilter>();
+
+//
+// Get the feature manager from application services
+using (ServiceProvider serviceProvider = services.BuildServiceProvider())
 {
-    class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            //
-            // Setup configuration
-            IConfiguration configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
+    IFeatureManager featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
 
-            //
-            // Setup application services + feature management
-            IServiceCollection services = new ServiceCollection();
-
-            services.AddSingleton(configuration)
-                    .AddFeatureManagement()
-                    .AddFeatureFilter<PercentageFilter>()
-                    .AddFeatureFilter<AccountIdFilter>();
-
-            //
-            // Get the feature manager from application services
-            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
-            {
-                IFeatureManager featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
-
-                var accounts = new List<string>()
+    var accounts = new List<string>()
                 {
                     "abc",
                     "adef",
                     "abcdefghijklmnopqrstuvwxyz"
                 };
 
-                //
-                // Mimic work items in a task-driven console application
-                foreach (var account in accounts)
-                {
-                    const string FeatureName = "Beta";
+    //
+    // Mimic work items in a task-driven console application
+    foreach (var account in accounts)
+    {
+        const string FeatureName = "Beta";
 
-                    //
-                    // Check if feature enabled
-                    //
-                    var accountServiceContext = new AccountServiceContext
-                    {
-                        AccountId = account
-                    };
+        //
+        // Check if feature enabled
+        //
+        var accountServiceContext = new AccountServiceContext
+        {
+            AccountId = account
+        };
 
-                    bool enabled = await featureManager.IsEnabledAsync(FeatureName, accountServiceContext);
+        bool enabled = await featureManager.IsEnabledAsync(FeatureName, accountServiceContext);
 
-                    //
-                    // Output results
-                    Console.WriteLine($"The {FeatureName} feature is {(enabled ? "enabled" : "disabled")} for the '{account}' account.");
-                }
-            }
-        }
+        //
+        // Output results
+        Console.WriteLine($"The {FeatureName} feature is {(enabled ? "enabled" : "disabled")} for the '{account}' account.");
     }
 }
--- examples/RazorPages/Program.cs
+++ examples/RazorPages/Program.cs
@@ -1,29 +1,28 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-//
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 
-namespace RazorPages
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddFeatureManagement();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
