aa3157e249152c4ed5838457a50ce50375fe094b
Ross Grambo <rossgrambo@microsoft.com>
2023-10-13 15:39:22 -0700

    Adds example library for Application Insights

---
+++ examples/EvaluationDataToApplicationInsights/Pages/Checkout.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EvaluationDataToApplicationInsights.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        private readonly ILogger<CheckoutModel> _logger;
+
+        public CheckoutModel(ILogger<CheckoutModel> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ examples/EvaluationDataToApplicationInsights/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.FeatureManagement;
+
+namespace EvaluationDataToApplicationInsights.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IFeatureManager _featureManager;
+
+        public IndexModel(ILogger<IndexModel> logger, IFeatureManager featureManager)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
