2380659af029e7a1d30db1bb5cbc2e37e59395f3
Jimmy Campbell <jimmyca@microsoft.com>
2023-11-07 11:09:20 -0800

    Updates.

--- examples/EvaluationDataToApplicationInsights/Pages/Index.cshtml.cs
+++ examples/EvaluationDataToApplicationInsights/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.FeatureManagement;
@@ -8,24 +7,61 @@ namespace EvaluationDataToApplicationInsights.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly IFeatureManager _featureManager;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IVariantFeatureManager _featureManager;
+        private readonly TelemetryClient _telemetry;
 
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, IFeatureManager featureManager, IHttpContextAccessor httpContextAccessor)
+        public IndexModel(
+            IVariantFeatureManager featureManager,
+            TelemetryClient telemetry)
         {
-            _logger = logger;
-            _featureManager = featureManager;
-            _httpContextAccessor = httpContextAccessor;
+            _featureManager = featureManager ?? throw new ArgumentNullException(nameof(featureManager));
+            _telemetry = telemetry ?? throw new ArgumentNullException(nameof(telemetry));
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGet()
         {
-            Username = _httpContextAccessor.HttpContext.Request.Cookies["username"];
+            Username = Request.Cookies["username"];
+
+            if (string.IsNullOrEmpty(Username))
+            {
+                return Redirect("/RandomizeUser");
+            }
+
+            //
+            // Use application's feature manager to get assigned variant for current user
+            //
+            Variant variant = await _featureManager
+                .GetVariantAsync("ImageRating", HttpContext.RequestAborted);
+
+            //
+            // Set the page's display image based on the assigned variant.
+            ViewData["ImageUri"] = variant.Configuration.Value;
 
             return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Request.Form != null)
+            {
+                string val = Request.Form["imageScore"];
+
+                if (val != null && 
+                    int.TryParse(val, out int rating))
+                {
+                    _telemetry.TrackEvent(
+                        "Vote",
+                        properties: null,
+                        metrics: new Dictionary<string, double>
+                        {
+                            { "ImageRating", rating }
+                        });
+                }
+            }
+
+            return Redirect("/RandomizeUser");
         }
     }
 }
--- examples/EvaluationDataToApplicationInsights/Program.cs
+++ examples/EvaluationDataToApplicationInsights/Program.cs
@@ -2,39 +2,61 @@
 // Licensed under the MIT license.
 //
 using Microsoft.FeatureManagement.Telemetry.ApplicationInsights;
-using EvaluationDataToApplicationInsights.Middlewares;
+using EvaluationDataToApplicationInsights.Telemetry;
 using Microsoft.FeatureManagement;
-using Microsoft.FeatureManagement.FeatureFilters;
 using EvaluationDataToApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//
+// Use Azure App Configuration as an application configuration provider
+//
+builder
+    .Configuration
+    .AddAzureAppConfiguration((options) =>
+    {
+        options.Connect(builder.Configuration["AzureAppConfigurationConnectionString"]);
+    });
+
+//
+// What a web app using app insights looks like
+//
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddApplicationInsightsTelemetry();
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddSingleton<ITargetingContextAccessor, HttpContextTargetingContextAccessor>();
-builder.Services.AddFeatureManagement()
-    .AddFeatureFilter<TargetingFilter>()
-    .AddTelemetryPublisher<ApplicationInsightsTelemetryPublisher>();
 
 //
 // App Insights User Tagging
 builder.Services.AddSingleton<ITelemetryInitializer, MyTelemetryInitializer>();
 
+//
+// Enter feature management
+//
+// Enhance a web application with feature management
+// Including user targeting capability
+// Wire up evaluation event emission
+builder.Services.AddFeatureManagement()
+    .WithTargeting<HttpContextTargetingContextAccessor>()
+    .AddTelemetryPublisher<ApplicationInsightsTelemetryPublisher>();
+
+//
+// Default code from .NET template below
+//
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
