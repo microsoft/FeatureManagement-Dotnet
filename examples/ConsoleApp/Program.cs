2f30c343b4e34bcb8d454ed7d143d08fd71158ac
zhiyuanliang <zhiyuanliang@microsoft.com>
2023-11-24 15:45:34 +0800

    fix bug

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FeatureManagement;
-using Microsoft.FeatureManagement.FeatureFilters;
 
 //
 // Setup configuration
