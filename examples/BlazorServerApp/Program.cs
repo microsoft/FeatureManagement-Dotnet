48f2a1ecb738d60fc4a57d8ebf4df1c70cb5f3f8
zhiyuanliang <zhiyuanliang@microsoft.com>
2023-12-21 13:36:19 +0800

    init

---
+++ examples/BlazorServerApp/Program.cs
@@ -0,0 +1,42 @@
+using BlazorServerApp.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.FeatureManagement;
+
+namespace BlazorServerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, RandomAuthenticationStateProvider>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
---
+++ examples/BlazorServerApp/RandomAuthenticationStateProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorServerApp
+{
+    public class RandomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            int randomNum = Random.Shared.Next();
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, randomNum.ToString())
+            };
+
+            ClaimsIdentity claimsIdentity;
+
+            if (randomNum % 2 == 0)
+            {
+                //
+                // If the random number is even, the user is considered as authenticated.
+                claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+            else
+            {
+                //
+                // If the random number is odd, the user is considered as not authenticated.
+                claimsIdentity = new ClaimsIdentity(claims);
+            }
+
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var state = new AuthenticationState(claimsPrincipal);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return Task.FromResult(state);
+        }
+    }
+}
