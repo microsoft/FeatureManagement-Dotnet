27ba646b14c9dd9329e6df3784a6983ba4cc87ed
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2024-01-11 09:54:57 +0800

    move public method to the front of private method (#349)

--- src/Microsoft.FeatureManagement/FeatureManagementBuilderExtensions.cs
+++ src/Microsoft.FeatureManagement/FeatureManagementBuilderExtensions.cs
@@ -17,6 +17,29 @@ namespace Microsoft.FeatureManagement
     public static class FeatureManagementBuilderExtensions
     {
         /// <summary>
+        /// Adds an <see cref="ITargetingContextAccessor"/> to be used for targeting and registers the targeting filter to the feature management system.
+        /// </summary>
+        /// <param name="builder">The <see cref="IFeatureManagementBuilder"/> used to customize feature management functionality.</param>
+        /// <returns>A <see cref="IFeatureManagementBuilder"/> that can be used to customize feature management functionality.</returns>
+        public static IFeatureManagementBuilder WithTargeting<T>(this IFeatureManagementBuilder builder) where T : ITargetingContextAccessor
+        {
+            //
+            // Register the targeting context accessor with the same lifetime as the feature manager
+            if (builder.Services.Any(descriptor => descriptor.ServiceType == typeof(IFeatureManager) && descriptor.Lifetime == ServiceLifetime.Scoped))
+            {
+                builder.Services.TryAddScoped(typeof(ITargetingContextAccessor), typeof(T));
+            }
+            else
+            {
+                builder.Services.TryAddSingleton(typeof(ITargetingContextAccessor), typeof(T));
+            }
+
+            builder.AddFeatureFilter<TargetingFilter>();
+
+            return builder;
+        }
+
+        /// <summary>
         /// Adds a telemetry publisher to the feature management system.
         /// </summary>
         /// <param name="builder">The <see cref="IFeatureManagementBuilder"/> used to customize feature management functionality.</param>
@@ -39,29 +62,6 @@ namespace Microsoft.FeatureManagement
 
                 options.TelemetryPublisherFactories.Add(factory);
             });
-
-            return builder;
-        }
-
-        /// <summary>
-        /// Adds an <see cref="ITargetingContextAccessor"/> to be used for targeting and registers the targeting filter to the feature management system.
-        /// </summary>
-        /// <param name="builder">The <see cref="IFeatureManagementBuilder"/> used to customize feature management functionality.</param>
-        /// <returns>A <see cref="IFeatureManagementBuilder"/> that can be used to customize feature management functionality.</returns>
-        public static IFeatureManagementBuilder WithTargeting<T>(this IFeatureManagementBuilder builder) where T : ITargetingContextAccessor
-        {
-            //
-            // Register the targeting context accessor with the same lifetime as the feature manager
-            if (builder.Services.Any(descriptor => descriptor.ServiceType == typeof(IFeatureManager) && descriptor.Lifetime == ServiceLifetime.Scoped))
-            {
-                builder.Services.TryAddScoped(typeof(ITargetingContextAccessor), typeof(T));
-            }
-            else
-            {
-                builder.Services.TryAddSingleton(typeof(ITargetingContextAccessor), typeof(T));
-            }
-
-            builder.AddFeatureFilter<TargetingFilter>();
 
             return builder;
         }
