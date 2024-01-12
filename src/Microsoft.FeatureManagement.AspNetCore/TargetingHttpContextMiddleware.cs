643a06df2907295bfbba9657d3172ca1a27ffc6a
Ross Grambo <rossgrambo@microsoft.com>
2024-01-11 16:10:14 -0800

    Adds targeting middleware and targeting initializer

---
+++ src/Microsoft.FeatureManagement.AspNetCore/TargetingHttpContextMiddleware.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.FeatureManagement.FeatureFilters;
+using System;
+using System.Threading.Tasks;
+
+namespace EvaluationDataToApplicationInsights.Telemetry
+{
+    /// <summary>
+    /// Used to add targeting information to http context. This allows synronous code to access targeting information.
+    /// </summary>
+    public class TargetingHttpContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// Creates an instance of the TargetingHttpContextMiddleware
+        /// </summary>
+        public TargetingHttpContextMiddleware(RequestDelegate next) { 
+            _next = next; 
+        }
+
+        /// <summary>
+        /// Adds targeting information to the http context.
+        /// </summary>
+        /// <param name="context">The <see cref="HttpContext"/> to add the targeting information to.</param>
+        /// <param name="targetingContextAccessor">The <see cref="ITargetingContextAccessor"/> to retrieve the targeting information from.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the provided context or targetingContextAccessor is null.</exception>
+        public async Task InvokeAsync(HttpContext context, ITargetingContextAccessor targetingContextAccessor)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (targetingContextAccessor == null)
+            {
+                throw new ArgumentNullException(nameof(targetingContextAccessor));
+            }
+
+            TargetingContext targetingContext = await targetingContextAccessor.GetContextAsync().ConfigureAwait(false);
+
+            if (targetingContext != null)
+            {
+                context.Items["TargetingId"] = targetingContext.UserId;
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ src/Microsoft.FeatureManagement.Telemetry.ApplicationInsights.AspNetCore/TargetingTelemetryInitializer.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+
+using Microsoft.ApplicationInsights.AspNetCore.TelemetryInitializers;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.FeatureManagement.Telemetry.ApplicationInsights.AspNetCore
+{
+    /// <summary>
+    /// Used to add targeting information to outgoing Application Insights telemetry.
+    /// </summary>
+    public class TargetingTelemetryInitializer : TelemetryInitializerBase
+    {
+        /// <summary>
+        /// Creates an instance of the TargetingTelemetryInitializer
+        /// </summary>
+        public TargetingTelemetryInitializer(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+
+        /// <summary>
+        /// When telemetry is initialized, adds targeting information to all relevant telemetry.
+        /// </summary>
+        /// <param name="httpContext">The <see cref="HttpContext"/> to get the targeting information from.</param>
+        /// <param name="requestTelemetry">The <see cref="RequestTelemetry"/> relevant to the telemetry.</param>
+        /// <param name="telemetry">The <see cref="ITelemetry"/> to be initialized.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the any param is null.</exception>
+        protected override void OnInitializeTelemetry(HttpContext httpContext, RequestTelemetry requestTelemetry, ITelemetry telemetry)
+        {
+            if (telemetry == null)
+            {
+                throw new ArgumentNullException("telemetry");
+            }
+
+            if (requestTelemetry == null)
+            {
+                throw new ArgumentNullException("requestTelemetry");
+            }
+
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException("httpContext");
+            }
+
+            string targetingId = (string) httpContext.Items["TargetingId"];
+
+            if (!string.IsNullOrEmpty(targetingId))
+            {
+                requestTelemetry.Properties["TargetingId"] = targetingId;
+
+                // Telemetry.Properties is deprecated in favor of ISupportProperties
+                if (telemetry is ISupportProperties telemetryWithSupportProperties)
+                {
+                    telemetryWithSupportProperties.Properties["TargetingId"] = targetingId;
+                }
+            }
+        }
+    }
+}
