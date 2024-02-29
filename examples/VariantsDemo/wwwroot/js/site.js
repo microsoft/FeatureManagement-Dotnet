0ad29b859f4af96ac6106bf9fd8ad957086c911f
Ross Grambo <rossgrambo@microsoft.com>
2024-02-29 15:25:27 -0800

    Additional tweaks

--- examples/VariantsDemo/wwwroot/js/site.js
+++ examples/VariantsDemo/wwwroot/js/site.js
@@ -87,6 +87,16 @@ function BindAndStoreData(container, index, data) {
         element: container.querySelector(".progress"),
         userInfo: data,
         progress: 0,
-        randomVariance: 1 + (.50 - Math.random() / 2)
+        randomVariance: 1 + gaussianRandom(0, 0.2)
     };
+}
+
+// Allows for a gaussian random number to be generated
+function gaussianRandom(mean = 0, stdev = 1) {
+    const u = 1 - Math.random(); // Converting [0, 1) to (0, 1]
+    const v = Math.random();
+    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+
+    // Transform to the desired mean and standard deviation:
+    return z * stdev + mean;
 }
