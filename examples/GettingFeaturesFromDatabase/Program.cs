4e98dec27a80c755d5412aed1ce3fbe001a37c16
A.F.M, Noorullah <n.a-f-m@itransition.com>
2024-03-06 20:06:46 +0600

    Remove unnecessary project references

--- examples/GettingFeaturesFromDatabase/Program.cs
+++ examples/GettingFeaturesFromDatabase/Program.cs
@@ -4,24 +4,15 @@ using Microsoft.FeatureManagement;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDatabase();
 builder.Services.AddFeatureService();
+
+/* Add feature management */
 builder.Services.AddScoped<IFeatureDefinitionProvider, CustomFeatureDefinitionProvider>().AddScopedFeatureManagement();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
