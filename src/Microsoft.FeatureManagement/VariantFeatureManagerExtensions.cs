5a8bf514a18f24e667046d85536e16ae2e3f2bd5
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2024-08-15 13:54:32 +0800

    update comment

--- src/Microsoft.FeatureManagement/VariantFeatureManagerExtensions.cs
+++ src/Microsoft.FeatureManagement/VariantFeatureManagerExtensions.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Microsoft.FeatureManagement
 {
     /// <summary>
-    /// Extensions used to provide an overload for <see cref="IVariantFeatureManager.GetVariantAsync"/> that accepts a <see cref="TargetingContext"/> type directly.
+    /// Extensions used to provide an overload for <see cref="IVariantFeatureManager.GetVariantAsync(string, ITargetingContext, CancellationToken)"/> that accepts a <see cref="TargetingContext"/> type directly.
     /// </summary>
     public static class VariantFeatureManagerExtensions
     {
