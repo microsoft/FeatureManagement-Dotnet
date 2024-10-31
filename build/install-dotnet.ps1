a077fc11ee73bbfeb0283ffc852ad3e340f3bded
Ross Grambo <rossgrambo@microsoft.com>
2024-10-31 13:13:30 -0700

    Adds .net 9 build to pipeline

--- build/install-dotnet.ps1
+++ build/install-dotnet.ps1
@@ -8,3 +8,5 @@
 &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel 7.0
 
 &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel 8.0
+
+&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel 9.0
