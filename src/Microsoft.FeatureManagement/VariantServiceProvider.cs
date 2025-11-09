6cc6320ab29830773dbeb695ff03d97c45a9acad
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-11-09 20:14:24 +0800

    lazy instantiation for variant service

--- src/Microsoft.FeatureManagement/VariantServiceProvider.cs
+++ src/Microsoft.FeatureManagement/VariantServiceProvider.cs
@@ -5,9 +5,9 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.FeatureManagement
 {
@@ -16,26 +16,55 @@ namespace Microsoft.FeatureManagement
     /// </summary>
     internal class VariantServiceProvider<TService> : IVariantServiceProvider<TService> where TService : class
     {
-        private readonly IEnumerable<TService> _services;
         private readonly IVariantFeatureManager _featureManager;
         private readonly string _featureName;
         private readonly ConcurrentDictionary<string, TService> _variantServiceCache;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly Dictionary<string, ServiceDescriptor> _variantNameToDescriptor; // ImplementationType/Instance descriptors mapped by variant name.
+        private readonly List<ServiceDescriptor> _factoryDescriptors; // Descriptors that require factory invocation to discover variant name.
 
         /// <summary>
         /// Creates a variant service provider.
         /// </summary>
         /// <param name="featureName">The feature flag that should be used to determine which variant of the service should be used.</param>
         /// <param name="featureManager">The feature manager to get the assigned variant of the feature flag.</param>
-        /// <param name="services">Implementation variants of TService.</param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="featureName"/> is null.</exception>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="featureManager"/> is null.</exception>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="services"/> is null.</exception>
-        public VariantServiceProvider(string featureName, IVariantFeatureManager featureManager, IEnumerable<TService> services)
+        /// <param name="serviceDescriptors">Service descriptors for implementation variants of TService.</param>
+        /// <param name="serviceProvider">The service provider / scope used to activate implementations lazily.</param>
+        public VariantServiceProvider(string featureName, IVariantFeatureManager featureManager, IEnumerable<ServiceDescriptor> serviceDescriptors, IServiceProvider serviceProvider)
         {
             _featureName = featureName ?? throw new ArgumentNullException(nameof(featureName));
             _featureManager = featureManager ?? throw new ArgumentNullException(nameof(featureManager));
-            _services = services ?? throw new ArgumentNullException(nameof(services));
-            _variantServiceCache = new ConcurrentDictionary<string, TService>();
+            if (serviceDescriptors == null) throw new ArgumentNullException(nameof(serviceDescriptors));
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _variantServiceCache = new ConcurrentDictionary<string, TService>(StringComparer.OrdinalIgnoreCase);
+            _variantNameToDescriptor = new Dictionary<string, ServiceDescriptor>(StringComparer.OrdinalIgnoreCase);
+            _factoryDescriptors = new List<ServiceDescriptor>();
+
+            // Precompute mapping for descriptors whose variant name can be determined without instantiation.
+            foreach (ServiceDescriptor descriptor in serviceDescriptors)
+            {
+                if (descriptor.ImplementationType != null)
+                {
+                    string name = GetVariantName(descriptor.ImplementationType);
+                    if (!_variantNameToDescriptor.ContainsKey(name))
+                    {
+                        _variantNameToDescriptor.Add(name, descriptor);
+                    }
+                }
+                else if (descriptor.ImplementationInstance != null)
+                {
+                    string name = GetVariantName(descriptor.ImplementationInstance.GetType());
+                    if (!_variantNameToDescriptor.ContainsKey(name))
+                    {
+                        _variantNameToDescriptor.Add(name, descriptor);
+                    }
+                }
+                else if (descriptor.ImplementationFactory != null)
+                {
+                    // Factory descriptors require instantiation to discover variant name; hold for later.
+                    _factoryDescriptors.Add(descriptor);
+                }
+            }
         }
 
         /// <summary>
@@ -47,25 +76,73 @@ namespace Microsoft.FeatureManagement
         {
             Debug.Assert(_featureName != null);
 
-            Variant variant = await _featureManager.GetVariantAsync(_featureName, cancellationToken);
+            Variant variant = await _featureManager.GetVariantAsync(_featureName, cancellationToken).ConfigureAwait(false);
 
-            TService implementation = null;
-
-            if (variant != null)
+            if (variant == null)
             {
-                implementation = _variantServiceCache.GetOrAdd(
-                    variant.Name,
-                    (_) => _services.FirstOrDefault(
-                        service => IsMatchingVariantName(
-                            service.GetType(),
-                            variant.Name))
-                );
+                return null;
             }
 
-            return implementation;
+            return _variantServiceCache.GetOrAdd(variant.Name, ResolveVariant);
         }
 
-        private bool IsMatchingVariantName(Type implementationType, string variantName)
+        private TService ResolveVariant(string variantName)
+        {
+            // Try fast path using precomputed mapping.
+            if (_variantNameToDescriptor.TryGetValue(variantName, out ServiceDescriptor descriptor))
+            {
+                return ActivateDescriptor(descriptor);
+            }
+
+            // Need to probe factory descriptors lazily.
+            foreach (ServiceDescriptor factoryDescriptor in _factoryDescriptors)
+            {
+                TService instance = ActivateDescriptor(factoryDescriptor);
+
+                if (instance == null)
+                {
+                    continue;
+                }
+
+                string discoveredName = GetVariantName(instance.GetType());
+
+                // Cache the mapping for future lookups.
+                if (!_variantNameToDescriptor.ContainsKey(discoveredName))
+                {
+                    _variantNameToDescriptor.Add(discoveredName, factoryDescriptor);
+                }
+
+                if (string.Equals(discoveredName, variantName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return instance;
+                }
+            }
+
+            return null;
+        }
+
+        private TService ActivateDescriptor(ServiceDescriptor descriptor)
+        {
+            if (descriptor.ImplementationInstance != null)
+            {
+                return (TService)descriptor.ImplementationInstance;
+            }
+
+            if (descriptor.ImplementationType != null)
+            {
+                // Use ActivatorUtilities to honor DI for dependencies of the implementation type.
+                return (TService)ActivatorUtilities.GetServiceOrCreateInstance(_serviceProvider, descriptor.ImplementationType);
+            }
+
+            if (descriptor.ImplementationFactory != null)
+            {
+                return (TService)descriptor.ImplementationFactory(_serviceProvider);
+            }
+
+            return null;
+        }
+
+        private string GetVariantName(Type implementationType)
         {
             string implementationName = ((VariantServiceAliasAttribute)Attribute.GetCustomAttribute(implementationType, typeof(VariantServiceAliasAttribute)))?.Alias;
 
@@ -74,7 +151,7 @@ namespace Microsoft.FeatureManagement
                 implementationName = implementationType.Name;
             }
 
-            return string.Equals(implementationName, variantName, StringComparison.OrdinalIgnoreCase);
+            return implementationName;
         }
     }
 }
